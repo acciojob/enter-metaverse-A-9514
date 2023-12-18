b8282bf56c000c96dc514ad7d92b7bda3ab8fdcd
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-18 20:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let enterBtn = document.querySelector("#enterBtn");
+enterBtn.addEventListener("click", addNewRow);
+
+function addNewRow(){
+    let consoleRow = document.querySelector(".console-row");
+    let consoleContainer = consoleRow.parentNode;
+    let outerContainer = consoleContainer.parentNode;
+	let angleRight = consoleRow.querySelector(".angle-right");
+
+	let madH1 = document.createElement("h1");
+
+    if(outerContainer.querySelector("#enterBtn").innerText == "Enter")
+        {
+			madH1.innerText = "Entered Metaverse";
+			consoleRow.querySelector("#status").innerText = "";
+            consoleRow.querySelector("#status").appendChild(madH1);
+            outerContainer.querySelector("#enterBtn").innerText = "Leave"
+        }
+    else if(outerContainer.querySelector("#enterBtn").innerText == "Leave")
+        {
+			madH1.innerText = "Enter Metaverse";
+			consoleRow.querySelector("#status").innerText = "";
+            consoleRow.querySelector("#status").appendChild(madH1);
+            outerContainer.querySelector("#enterBtn").innerText = "Enter";
+        }
+    
+}
